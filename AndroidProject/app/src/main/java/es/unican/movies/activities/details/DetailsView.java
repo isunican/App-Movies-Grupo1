281b763a260a6a892728fd718b29366d4059fc01
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-10-07 16:34:12 +0200

    Logica de negocio para Detalles de la Pelicula

--- AndroidProject/app/src/main/java/es/unican/movies/activities/details/DetailsView.java
+++ AndroidProject/app/src/main/java/es/unican/movies/activities/details/DetailsView.java
@@ -1,34 +1,73 @@
 package es.unican.movies.activities.details;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
 import es.unican.movies.R;
 import es.unican.movies.model.Movie;
+import es.unican.movies.service.EImageSize;
+import es.unican.movies.service.ITmdbApi;
 
-/**
- * Activity to show the details of a movie.
- */
-public class DetailsView extends AppCompatActivity {
+public class DetailsView extends AppCompatActivity implements IDetailsContract.View {
 
     public static final String INTENT_MOVIE = "INTENT_MOVIE";
 
+    private IDetailsContract.Presenter presenter;
+
+    private ImageView ivPoster;
+    private TextView tvTitle;
+    private TextView tvReleaseYear;
+    private TextView tvDuration;
+    private TextView tvGenres;
+    private TextView tvVoteAverage;
+    private TextView tvSummaryScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        Movie movie = Parcels.unwrap(getIntent().getExtras().getParcelable(INTENT_MOVIE));
-        assert movie != null;
-        TextView tvTitle = findViewById(R.id.tvTitle);
-        TextView tvId = findViewById(R.id.tvId);
-        tvTitle.setText(movie.getTitle());
-        tvId.setText(String.valueOf(movie.getId()));
-        
+        // Link UI elements
+        ivPoster = findViewById(R.id.ivPoster);
+        tvTitle = findViewById(R.id.tvTitle);
+        tvReleaseYear = findViewById(R.id.tvReleaseYear);
+        tvDuration = findViewById(R.id.tvDuration);
+        tvGenres = findViewById(R.id.tvGenres);
+        tvVoteAverage = findViewById(R.id.tvVoteAverage);
+        tvSummaryScore = findViewById(R.id.tvSummaryScore);
+
+        // Create presenter and init
+        presenter = new DetailsPresenter(this);
+        presenter.init();
+    }
+
+    @Override
+    public Movie getMovie() {
+        // Get movie from intent
+        return Parcels.unwrap(getIntent().getParcelableExtra(INTENT_MOVIE));
+    }
+
+    @Override
+    public void showMovieInfo(String title, String releaseDate, String duration, String genres, String voteAverage, String summaryScore, String posterPath) {
+        tvTitle.setText(title);
+        tvReleaseYear.setText(releaseDate);
+        tvDuration.setText(duration);
+        tvGenres.setText(genres);
+        tvVoteAverage.setText(voteAverage);
+        tvSummaryScore.setText(summaryScore);
+
+        if (posterPath != null && !"-".equals(posterPath)) {
+            String imageUrl = ITmdbApi.getFullImagePath(posterPath, EImageSize.W342);
+            Picasso.get().load(imageUrl).into(ivPoster);
+        } else {
+            // Set a placeholder image if the poster is not available
+            ivPoster.setImageResource(R.drawable.ic_launcher_background); // Asegúrate de tener un drawable para esto
+        }
     }
 }
