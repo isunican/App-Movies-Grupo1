894ac2886ad0be6a47777eee984ef1087de13bd8
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-10-15 18:04:54 +0200

    Logica de negocio para el menu de filtrado por generos, basico, sin validaciones especificas

--- AndroidProject/app/src/main/java/es/unican/movies/activities/main/MainView.java
+++ AndroidProject/app/src/main/java/es/unican/movies/activities/main/MainView.java
@@ -1,5 +1,6 @@
 package es.unican.movies.activities.main;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,12 +10,16 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -25,40 +30,24 @@ import es.unican.movies.activities.info.InfoActivity;
 import es.unican.movies.model.Movie;
 import es.unican.movies.service.IMoviesRepository;
 
-/**
- * Activity to show the list of movies.
- */
 @AndroidEntryPoint
 public class MainView extends AppCompatActivity implements IMainContract.View {
 
-    /**
-     * Presenter that will take control of this view.
-     */
     private IMainContract.Presenter presenter;
 
-    /**
-     * Repository that can be used to retrieve movies or series.
-     */
     @Inject
     IMoviesRepository repository;
 
-    /**
-     * Reference to the ListView that shows the list of movies
-     */
-    private ListView lvMovies; // Added ListView member
+    private ListView lvMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // The default theme does not include a toolbar.
-        // In this app the toolbar is explicitly declared in the layout
-        // This sets this toolbar as the activity ActionBar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // instantiate presenter, let it take control
         presenter = new MainPresenter();
         presenter.init(this);
     }
@@ -70,17 +59,17 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
         return true;
     }
 
-    /**
-     * This is called when an item in the action bar menu is selected.
-     * @param item The menu item that was selected.
-     *
-     * @return true if we have handled the selection
-     */
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.menuItemInfo) {
             presenter.onMenuInfoClicked();
+            return true;
+        } else if (itemId == R.id.menuItemFilterGenre) {
+            presenter.onFilterMenuClicked();
+            return true;
+        } else if (itemId == R.id.menuItemFilterDecade) {
+            Toast.makeText(this, "Esta funcionalidad estará disponible próximamente", Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -90,8 +79,7 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
     public void init() {
         lvMovies = findViewById(R.id.lvMovies);
         lvMovies.setOnItemClickListener((parent, view, position, id) -> {
-            Movie movie = (Movie) parent.getItemAtPosition(position);
-            presenter.onItemClicked(movie);
+            presenter.onItemClicked((Movie) parent.getItemAtPosition(position));
         });
     }
 
@@ -108,12 +96,13 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
 
     @Override
     public void showLoadCorrect(int movies) {
-        Toast.makeText(this, "Loaded " + movies + " movies", Toast.LENGTH_SHORT).show();
+        String text = String.format("Se cargaron %d películas", movies);
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void showLoadError() {
-        Toast.makeText(this, "Error loading movies", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Error al cargar las películas", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -126,5 +115,52 @@ public class MainView extends AppCompatActivity implements IMainContract.View {
     @Override
     public void showInfoActivity() {
         startActivity(new Intent(this, InfoActivity.class));
+    }
+
+    @Override
+    public void showGenreFilterDialog(List<String> allGenres, List<String> selectedGenres) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Filtrar por Género");
+
+        final CharSequence[] genreNames = allGenres.toArray(new CharSequence[0]);
+        final boolean[] checkedItems = new boolean[genreNames.length];
+        final Set<String> tempSelected = new HashSet<>(selectedGenres);
+
+        for (int i = 0; i < genreNames.length; i++) {
+            checkedItems[i] = tempSelected.contains(genreNames[i].toString());
+        }
+
+        builder.setMultiChoiceItems(genreNames, checkedItems, (dialog, which, isChecked) -> {
+            String genre = genreNames[which].toString();
+            if (isChecked) {
+                tempSelected.add(genre);
+            } else {
+                tempSelected.remove(genre);
+            }
+            AlertDialog d = (AlertDialog) dialog;
+            boolean changed = !tempSelected.equals(new HashSet<>(selectedGenres));
+            d.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(changed);
+        });
+
+        builder.setPositiveButton("APLICAR", (dialog, which) -> {
+            presenter.onGenresFiltered(new ArrayList<>(tempSelected));
+        });
+
+        builder.setNegativeButton("CANCELAR", (dialog, which) -> dialog.dismiss());
+
+        builder.setNeutralButton("LIMPIAR", (dialog, which) -> {
+            if (!selectedGenres.isEmpty()) {
+                presenter.onGenresFiltered(new ArrayList<>());
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+
+        dialog.setOnShowListener(d -> {
+            dialog.getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+            dialog.getButton(DialogInterface.BUTTON_NEUTRAL).setEnabled(!selectedGenres.isEmpty());
+        });
+
+        dialog.show();
     }
 }
