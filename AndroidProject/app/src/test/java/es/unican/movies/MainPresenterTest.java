2d9a4c80195d676aa9a52ca85f60f14046640c36
Sergio <barredamenocalsergio@gmail.com>
2025-10-20 17:06:49 +0200

    Creada clase test unitarios

---
+++ AndroidProject/app/src/test/java/es/unican/movies/MainPresenterTest.java
@@ -0,0 +1,14 @@
+package es.unican.movies;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainPresenterTest {
+
+
+}
