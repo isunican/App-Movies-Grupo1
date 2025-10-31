25e4140e9deb98b47bc65c277e53d3e42638c09a
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-10-31 16:26:56 +0100

    Actualizada la base de datos Room

---
+++ AndroidProject/app/src/main/java/es/unican/movies/data/AppDatabase.java
@@ -0,0 +1,15 @@
+package es.unican.movies.data;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import es.unican.movies.model.MovieInList;
+import es.unican.movies.model.MovieInListDao;
+
+/**
+ * Clase de la base de datos Room.
+ */
+@Database(entities = {MovieInList.class}, version = 3, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract MovieInListDao movieInListDao();
+}
