e75f45097012a1c3c6eccb8ac1325c8528f2c6c6
Carlos Andres <sandovalcarlos@uninorte.edu.co>
2025-10-31 16:31:17 +0100

    Refactor del módulo de inyección de la base de datos

--- AndroidProject/app/src/main/java/es/unican/movies/injection/DatabaseModule.java
+++ AndroidProject/app/src/main/java/es/unican/movies/injection/DatabaseModule.java
@@ -1,8 +1,11 @@
 package es.unican.movies.injection;
 
 import android.app.Application;
+
 import androidx.room.Room;
+
 import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
@@ -11,23 +14,36 @@ import es.unican.movies.data.AppDatabase;
 import es.unican.movies.model.MovieInListDao;
 
 /**
- * Módulo de Hilt para proveer la instancia de la base de datos Room y el DAO.
+ * Módulo de inyección de dependencias para la base de datos de Room.
  */
 @Module
 @InstallIn(SingletonComponent.class)
 public class DatabaseModule {
 
+    /**
+     * Provee una instancia única de la base de datos de la aplicación.
+     *
+     * @param application La aplicación.
+     * @return La instancia de la base de datos.
+     */
     @Provides
     @Singleton
-    public AppDatabase provideDatabase(Application application) {
-        return Room.databaseBuilder(application, AppDatabase.class, "movies-db")
+    public AppDatabase provideAppDatabase(Application application) {
+        // Changing the database name to force a recreation
+        return Room.databaseBuilder(application, AppDatabase.class, "movies-db-v2")
                 .fallbackToDestructiveMigration()
                 .build();
     }
 
+    /**
+     * Provee una instancia del DAO para las películas en la lista del usuario.
+     *
+     * @param appDatabase La base de datos de la aplicación.
+     * @return El DAO de películas en la lista.
+     */
     @Provides
-    public MovieInListDao provideMovieInListDao(AppDatabase database) {
-        return database.movieInListDao();
+    @Singleton
+    public MovieInListDao provideMovieInListDao(AppDatabase appDatabase) {
+        return appDatabase.movieInListDao();
     }
-
 }
